92c7c63e6a95699cc4ef2d7fb834c218fe1791ab
Aliya Salmanova <aliyasalmanova1@gmail.com>
2026-09-15 18:44:37 -0400

    placed mainmenu fonts and temp assets

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+
+    [SerializeField] private Color hoverColor;
+
+    [SerializeField] private Color originalColor;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    void OnHover()
+    {
+        
+    }
+}
